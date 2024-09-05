eee51e3a9ed0a4c947cfd55711ee8546d051ac89
sharmilakumaravel0 <sharmilakumaravel03082003@gmail.com>
2024-09-05 22:57:47 +0530

    git commit-m "sample"

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Components/Home';
-import About from './Components/About';
-import Nav from './Components/Nav';
-import "./CSS/App.css"
+import Header from './Components/header/Header'
+
+import "./Components/carousel/Carousel.css"
+import "./Components/eventinfo/EventInfo.css"
+import "./Components/footer/Footer.css"
+import "./Components/header/Header.css"
+import "./Components/eventproviders/EventProviders.css"
+import Home from './Components/eventproviders/EventProviders'
+import About from './Components/about/About'
+import StudentRegistration from './Components/studentregistration/StudentRegistration';
+import ContactUs from './Components/contactus/ContactUs';
+import Login from './Components/login/Login';
+import CollegeList from './Components/CollegeList/collegeList';
+import Events from './Components/events/Events';
+import JobFairs from './Components/jobfair/JobFair';
+import "./Components/studentregistration/StudentRegistration.css";
+import "./Components/contactus/ContactUs.css"
+import "./Components/login/Login.css"
+import "./Components/events/Events.css"
+import "./Components/herosection/HeroSection.css"
+
+import "./Components/jobfair/JobFair.css";
+
+
 
 
 
@@ -11,11 +31,21 @@ import "./CSS/App.css"
 function App() {
   return (
     <Router>
-      <Nav />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-      
+        <Route path="/studentregistration" element={<StudentRegistration />} />
+        <Route path="/contactus" element={<ContactUs />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/colleges" element={<CollegeList />} />
+        <Route path="/" element={<Events />} />
+        <Route path="/jobfair" element={<JobFairs />} />
+        
+
+       
+       
+       
       </Routes>
     
     </Router>
